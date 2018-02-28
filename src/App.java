e53d3e5263f28dc8accd06a655a50169c34deb3b
Namdrib <denton_the_bird_man@hotmail.com>
2018-02-28 23:45:29 +1030

    Added bot playing

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -15,11 +16,13 @@ public class App extends ListenerAdapter
 	public final static String botToken = "YOUR_BOT_TOKEN_HERE";
 	public static void main(String[] args) throws LoginException, IllegalArgumentException, RateLimitedException
 	{
-		@SuppressWarnings("unused")
 		JDA api = new JDABuilder(AccountType.BOT)
 				.setToken(App.botToken)
 				.addEventListener(new App())
 				.buildAsync();
+
+		// Set the game to a useful message
+		api.getPresence().setGame(Game.playing("Invoke with " + Global.prefix));
 	}
 
 	@Override
--- src/Parser.java
+++ src/Parser.java
@@ -79,7 +79,8 @@ public class Parser
 		System.out.println("Creating role with name " + name);
 		try
 		{
-			guildController.createRole().setName(name).queue(x -> x.getManager().setMentionable(true).queue());
+			guildController.createRole().setName(name)
+			.queue(x -> x.getManager().setMentionable(true).queue());
 		}
 		catch (Exception ex)
 		{
@@ -87,7 +88,7 @@ public class Parser
 		}
 		System.out.println("Done");
 	}
-	
+
 	private <E> String listWithoutBrackets(List<E> items)
 	{
 		String s = items.toString();
@@ -113,20 +114,20 @@ public class Parser
 	private void help()
 	{
 		String helpMessage
-				= " ----- " + Global.botName + " help -----\n"
-				+ "Invoke the bot using `" + Global.prefix + "` followed by one of the following commands:\n"
-				+ "  `help`: display this help message\n"
-				+ "  `list`: list your own roles\n"
-				+ "  `listAll`: list all available roles you can add to yourself\n"
-				+ "  `addRole ROLE`: add `ROLE` to yourself (where `ROLE` is in `listAll`)\n"
-				+ "  `addRoles ROLES...`: add `ROLES...` to yourself (where `ROLES...` are in `listAll`)\n"
-				+ "  `removeRole ROLE`: remove `ROLE` from yourself (where `ROLE` is in `list`)\n"
-				+ "  `removeRoles ROLES...`: remove `ROLES...` from yourself (where `ROLES...` are in `listAll`)\n"
-				+ "  `removeAllRoles`: remove all roles from yourself\n"
-				+ "  `createRole ROLE`: create a role with name `ROLE`\n"
-				+ "  `createRoles ROLES...`: create multiple roles with names `ROLES...`\n"
-				+ "  `membersWith ROLE`: list all members to whom ROLE is assigned\n"
-				;
+			= " ----- " + Global.botName + " help -----\n"
+			+ "Invoke the bot using `" + Global.prefix + "` followed by one of the following commands:\n"
+			+ "  `help`: display this help message\n"
+			+ "  `list`: list your own roles\n"
+			+ "  `listAll`: list all available roles you can add to yourself\n"
+			+ "  `addRole ROLE`: add `ROLE` to yourself (where `ROLE` is in `listAll`)\n"
+			+ "  `addRoles ROLES...`: add `ROLES...` to yourself (where `ROLES...` are in `listAll`)\n"
+			+ "  `removeRole ROLE`: remove `ROLE` from yourself (where `ROLE` is in `list`)\n"
+			+ "  `removeRoles ROLES...`: remove `ROLES...` from yourself (where `ROLES...` are in `listAll`)\n"
+			+ "  `removeAllRoles`: remove all roles from yourself\n"
+			+ "  `createRole ROLE`: create a role with name `ROLE`\n"
+			+ "  `createRoles ROLES...`: create multiple roles with names `ROLES...`\n"
+			+ "  `membersWith ROLE`: list all members to whom ROLE is assigned\n"
+		;
 
 		send(helpMessage);
 	}
@@ -150,272 +151,273 @@ public class Parser
 		String command = st.nextToken().toLowerCase();
 		switch (command)
 		{
-			case "help":
+		case "help":
+		{
+			send("`help` command invoked");
+			help();
+			break;
+		}
+
+		case "list":
+		{
+			send("`list` command invoked");
+
+			try
 			{
-				send("`help` command invoked");
-				help();
-				break;
+				List<Role> memberRoles = new ArrayList<>(member.getRoles());
+				memberRoles.remove(guild.getPublicRole());
+
+				if (memberRoles.isEmpty())
+				{
+					send(tagMember() + " has no roles");
+					return;
+				}
+
+				String listMessage = "List of current roles for " + tagMember() + "\n";
+				outputListOfRoles(memberRoles, listMessage);
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+			break;
+		}
+		case "listall":
+		{
+			send("`listAll` command invoked");
+
+			List<Role> allRoles = new ArrayList<>(guild.getRoles());
+			allRoles.remove(guild.getPublicRole());
+
+			String listAllMessage = "List of all available roles\n";
+			outputListOfRoles(allRoles, listAllMessage);
+			break;
+		}
+
+		case "addrole":
+		{
+			send("`addRole` command invoked");
+
+			String argument = st.nextToken();
+			if (argument == null)
+			{
+				send("Usage: `addRole role`");
+				return;
 			}
 
-			case "list":
+			try
 			{
-				send("`list` command invoked");
+				List<Role> roles = guild.getRolesByName(argument, true);
+				if (roles.isEmpty())
+				{
+					send("Role " + argument + " does not exist. Maybe try creating it first");
+					return;
+				}
+				Role r = roles.get(0);
+				send("Adding " + r.getName());
+				guildController.addSingleRoleToMember(member, r).queue();
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+			break;
+		}
+		case "addroles":
+		{
+			send("`addRoles` command invoked");
 
+			String argument;
+			List<Role> rolesToAdd = new ArrayList<>();
+			final List<Role> allRoles = guild.getRoles();
+			while (st.hasMoreTokens())
+			{
+				argument = st.nextToken();
+				for (Role r : allRoles)
+				{
+					if (argument.equals(r.getName()))
+					{
+						rolesToAdd.add(r);
+						break;
+					}
+				}
+			}
+
+			if (rolesToAdd.isEmpty())
+			{
+				send("No roles to add");
+				return;
+			}
+
+			send("Adding " + rolesToAdd);
+			try
+			{
+				guildController.addRolesToMember(member, rolesToAdd).queue();
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+			break;
+		}
+
+		case "removerole":
+		{
+			send("`removeRole` command invoked");
+
+			String argument = st.nextToken();
+			if (argument == null)
+			{
+				send("Usage: `removeRole role`");
+				return;
+			}
+
+			try
+			{
+				List<Role> roles = guild.getRolesByName(argument, true);
+				if (roles.isEmpty())
+				{
+					send(argument + " is not assigned to " + member.getEffectiveName());
+					return;
+				}
+				Role roleToRemove = roles.get(0);
+				send("Removing " + roleToRemove.getName());
+				guildController.removeSingleRoleFromMember(member, roleToRemove).queue();
+			}
+			catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+			break;
+		}
+		case "removeroles":
+		{
+			send("`removeRoles` command invoked");
+
+			String argument;
+			List<Role> rolesToRemove = new ArrayList<>();
+			final List<Role> potentialRoles = member.getRoles();
+			if (potentialRoles.isEmpty())
+			{
+				send("No roles to remove");
+				return;
+			}
+
+			while (st.hasMoreTokens())
+			{
+				argument = st.nextToken();
+				for (Role r : potentialRoles)
+				{
+					if (argument.equals(r.getName()))
+					{
+						rolesToRemove.add(r);
+						break;
+					}
+				}
+			}
+
+			if (rolesToRemove.isEmpty())
+			{
+				send("No roles to remove");
+				return;
+			}
+
+			send("Removing " + rolesToRemove);
+			guildController.removeRolesFromMember(member, rolesToRemove).queue();
+			break;
+		}
+		case "removeallroles":
+		{
+			send("`removeAllRoles` command invoked");
+
+			List<Role> roles = new ArrayList<>(member.getRoles());
+			List<String> removed = new ArrayList<>();
+			for (Role r : roles)
+			{
 				try
 				{
-					List<Role> memberRoles = new ArrayList<>(member.getRoles());
-					memberRoles.remove(guild.getPublicRole());
-
-					if (memberRoles.isEmpty())
-					{
-						send(tagMember() + " has no roles");
-						return;
-					}
-
-					String listMessage = "List of current roles for " + tagMember() + "\n";
-					outputListOfRoles(memberRoles, listMessage);
+					guildController.removeSingleRoleFromMember(member, r).queue();
+					removed.add(r.getName());
 				}
 				catch (Exception ex)
 				{
 					ex.printStackTrace();
 				}
-				break;
 			}
-			case "listall":
+			String out = "Roles removed from " + member.getEffectiveName()
+					+ ": ";
+			out += listWithoutBrackets(removed);
+			send(out);
+			break;
+		}
+
+		case "createrole":
+		{
+			send("`createRole` command invoked");
+
+			String argument = st.nextToken();
+			if (argument == null)
 			{
-				send("`listAll` command invoked");
-
-				List<Role> allRoles = new ArrayList<>(guild.getRoles());
-				allRoles.remove(guild.getPublicRole());
-
-				String listAllMessage = "List of all available roles\n";
-				outputListOfRoles(allRoles, listAllMessage);
-				break;
-			}
-
-			case "addrole":
-			{
-				send("`addRole` command invoked");
-
-				String argument = st.nextToken();
-				if (argument == null)
-				{
-					send("Usage: `addRole role`");
-					return;
-				}
-				
-				try
-				{
-					List<Role> roles = guild.getRolesByName(argument, true);
-					if (roles.isEmpty())
-					{
-						send("Role " + argument + " does not exist. Maybe try creating it first");
-						return;
-					}
-					Role r = roles.get(0);
-					send("Adding " + r.getName());
-					guildController.addSingleRoleToMember(member, r).queue();
-				}
-				catch (Exception ex)
-				{
-					ex.printStackTrace();
-				}
-				break;
-			}
-			case "addroles":
-			{
-				send("`addRoles` command invoked");
-
-				String argument;
-				List<Role> rolesToAdd = new ArrayList<>();
-				final List<Role> allRoles = guild.getRoles();
-				while (st.hasMoreTokens())
-				{
-					argument = st.nextToken();
-					for (Role r : allRoles)
-					{
-						if (argument.equals(r.getName()))
-						{
-							rolesToAdd.add(r);
-							break;
-						}
-					}
-				}
-
-				if (rolesToAdd.isEmpty())
-				{
-					send("No roles to add");
-					return;
-				}
-
-				send("Adding " + rolesToAdd);
-				try
-				{
-					guildController.addRolesToMember(member, rolesToAdd).queue();
-				}
-				catch (Exception ex)
-				{
-					ex.printStackTrace();
-				}
-				break;
+				send("Usage: `createRole role`");
+				return;
 			}
 
-			case "removerole":
+			createMentionableRole(argument);
+			break;
+		}
+		case "createroles":
+		{
+			send("`createRoles` command invoked");
+
+			String argument;
+			while (st.hasMoreTokens())
 			{
-				send("`removeRole` command invoked");
-
-				String argument = st.nextToken();
-				if (argument == null)
-				{
-					send("Usage: `removeRole role`");
-					return;
-				}
-
-				try
-				{
-					List<Role> roles = guild.getRolesByName(argument, true);
-					if (roles.isEmpty())
-					{
-						send(argument + " is not assigned to " + member.getEffectiveName());
-						return;
-					}
-					Role roleToRemove = roles.get(0);
-					send("Removing " + roleToRemove.getName());
-					guildController.removeSingleRoleFromMember(member, roleToRemove).queue();
-				}
-				catch (Exception ex)
-				{
-					ex.printStackTrace();
-				}
-				break;
-			}
-			case "removeroles":
-			{
-				send("`removeRoles` command invoked");
-
-				String argument;
-				List<Role> rolesToRemove = new ArrayList<>();
-				final List<Role> potentialRoles = member.getRoles();
-				if (potentialRoles.isEmpty())
-				{
-					send("No roles to remove");
-					return;
-				}
-
-				while (st.hasMoreTokens())
-				{
-					argument = st.nextToken();
-					for (Role r : potentialRoles)
-					{
-						if (argument.equals(r.getName()))
-						{
-							rolesToRemove.add(r);
-							break;
-						}
-					}
-				}
-
-				if (rolesToRemove.isEmpty())
-				{
-					send("No roles to remove");
-					return;
-				}
-
-				send("Removing " + rolesToRemove);
-				guildController.removeRolesFromMember(member, rolesToRemove).queue();
-				break;
-			}
-			case "removeallroles":
-			{
-				send("`removeAllRoles` command invoked");
-
-				List<Role> roles = new ArrayList<>(member.getRoles());
-				List<String> removed = new ArrayList<>();
-				for (Role r : roles)
-				{
-					try
-					{
-						guildController.removeSingleRoleFromMember(member, r).queue();
-						removed.add(r.getName());
-					}
-					catch (Exception ex)
-					{
-						ex.printStackTrace();
-					}
-				}
-				String out = "Roles removed from " + member.getEffectiveName() + ": ";
-				out += listWithoutBrackets(removed);
-				send(out);
-				break;
-			}
-
-			case "createrole":
-			{
-				send("`createRole` command invoked");
-
-				String argument = st.nextToken();
-				if (argument == null)
-				{
-					send("Usage: `createRole role`");
-					return;
-				}
-				
+				argument = st.nextToken();
 				createMentionableRole(argument);
-				break;
 			}
-			case "createroles":
+
+			break;
+		}
+
+		case "memberswith":
+		{
+			send("`membersWith` command invoked");
+
+			String argument = st.nextToken();
+			if (argument == null)
 			{
-				send("`createRoles` command invoked");
-
-				String argument;
-				while (st.hasMoreTokens())
-				{
-					argument = st.nextToken();
-					createMentionableRole(argument);
-				}
-
-				break;
+				send("Usage: `membersWith role`");
+				return;
 			}
-			
-			case "memberswith":
+
+			List<Member> members = guild
+					.getMembersWithRoles(guild.getRolesByName(argument, true));
+			if (members.isEmpty())
 			{
-				send("`membersWith` command invoked");
-
-				String argument = st.nextToken();
-				if (argument == null)
-				{
-					send("Usage: `membersWith role`");
-					return;
-				}
-
-				List<Member> members = guild
-						.getMembersWithRoles(guild.getRolesByName(argument, true));
-				if (members.isEmpty())
-				{
-					send("No members with role " + argument);
-					return;
-				}
-
-				Collections.sort(members, new Comparator<Member>() {
-					@Override
-					public int compare(Member a, Member b)
-					{
-						return a.getEffectiveName().compareTo(b.getEffectiveName());
-					}
-				});
-				String out = "Members with role " + argument + ":\n";
-				List<String> namesList = members.stream()
-						.map(Member::getEffectiveName).collect(Collectors.toList());
-				out += listWithoutBrackets(namesList);
-				send(out);
-				break;
+				send("No members with role " + argument);
+				return;
 			}
-			
-			default:
-			{
-				send("invalid command invoked");
-				help();
-			}
+
+			Collections.sort(members, new Comparator<Member>() {
+				@Override
+				public int compare(Member a, Member b)
+				{
+					return a.getEffectiveName().compareTo(b.getEffectiveName());
+				}
+			});
+			String out = "Members with role " + argument + ":\n";
+			List<String> namesList = members.stream()
+					.map(Member::getEffectiveName).collect(Collectors.toList());
+			out += listWithoutBrackets(namesList);
+			send(out);
+			break;
+		}
+
+		default:
+		{
+			send("invalid command invoked");
+			help();
+		}
 		}
 	}
 }
